cb5f54d528a6828d9d1391d9346a197478c6d69c
emrankhn <atdimla@gmail.com>
2022-03-11 20:09:52 +0600

    first commit

---
+++ src/java/com/jsf/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package com.jsf.dao;
+
+import com.jsf.model.User;
+import com.jsf.util.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "userDAO")
+public class UserDAO {
+   
+
+}
